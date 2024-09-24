76caddcc0ce1c560e9431f0abc13e6d2b2ef6360
milindbachani <milind.bachani45@gmail.com>
2024-09-24 12:12:15 +0530

    Breaking change

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -35,6 +35,8 @@ import java.util.Optional;
 @Model(adaptables = Resource.class)
 public class HelloWorldModel {
 
+    public static final String PASSWORD="mypass";
+
     @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
     @Default(values="No resourceType")
     protected String resourceType;
